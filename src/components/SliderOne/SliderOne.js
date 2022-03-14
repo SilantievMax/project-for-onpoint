fe2985f4726ff90c2f7394cb3c67aa22e2f7b1f3
SilantievMax <nohrin.max@yandex.ru>
2022-03-15 01:17:42 +0300

    added images to the first slide

--- src/components/SliderOne/SliderOne.js
+++ src/components/SliderOne/SliderOne.js
@@ -1,5 +1,15 @@
 import React from "react";
 import './SliderOne.css'
+import Layer1 from '../../img/Layer1.png'
+import Layer2 from '../../img/Layer2.png'
+import Layer3 from '../../img/Layer3.png'
+import Layer4 from '../../img/Layer4.png'
+import Layer5 from '../../img/Layer5.png'
+import Layer6 from '../../img/Layer6.png'
+import Layer7 from '../../img/Layer7.png'
+import Layer8 from '../../img/Layer8.png'
+import Layer9 from '../../img/Layer9.png'
+
 
 export default function SliderOne() { 
     return (
@@ -11,6 +21,17 @@ export default function SliderOne() {
                     <div className="slider-one-il">&#10132;</div>
                     <h6 className="slider-one-il">Что дальше?</h6>
                 </button>
+            </div>
+            <div className="slider-one-img">
+                <img src={Layer1} className="slider-one-img-layer1 slider-one-abs" alt="slider-one-img-layer1"/>
+                <img src={Layer2} className="slider-one-img-layer2 slider-one-abs" alt="slider-one-img-layer2"/>
+                <img src={Layer3} className="slider-one-img-layer3 slider-one-abs" alt="slider-one-img-layer3"/>
+                <img src={Layer4} className="slider-one-img-layer4 slider-one-abs" alt="slider-one-img-Layer4"/>
+                <img src={Layer5} className="slider-one-img-layer5 slider-one-abs" alt="slider-one-img-layer5"/>
+                <img src={Layer6} className="slider-one-img-layer6 slider-one-abs" alt="slider-one-img-layer6"/>
+                <img src={Layer7} className="slider-one-img-layer7 slider-one-abs" alt="slider-one-img-layer7"/>
+                <img src={Layer8} className="slider-one-img-layer8 slider-one-abs" alt="slider-one-img-layer8"/>
+                <img src={Layer9} className="slider-one-img-layer9 slider-one-abs" alt="slider-one-img-layer9"/>
             </div>
         </div>
     );
